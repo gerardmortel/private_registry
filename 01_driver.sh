1e8dc22dd50223e20f068ecd4aed34f63a862d40
Gerard Mortel <gmortel@us.ibm.com>
2023-10-10 13:04:17 -0500

    Do not configure allowed registries

--- 01_driver.sh
+++ 01_driver.sh
@@ -6,5 +6,5 @@ echo "#### Running the driver file"
 ./04_Configure_Host_to_Mirror_Images.sh
 ./05_Download_CASE_files.sh
 ./06_Mirror_Images_to_Private_Registry.sh
-./07_Configure_Secure_and_Allowed_Registries.sh
+# ./07_Configure_Secure_and_Allowed_Registries.sh
 ./08_Install_Catalog_and_Operator.sh
