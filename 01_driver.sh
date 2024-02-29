96b0d9299db76a1685cd2a612e904932667b446c
Gerard Mortel <gmortel@us.ibm.com>
2024-02-29 10:30:33 -0600

    Test mirror first

--- 01_driver.sh
+++ 01_driver.sh
@@ -6,6 +6,6 @@ echo "#### Running the driver file"
 ./04_Set_up_a_private_registry_on_Podman.sh
 ./05_Download_CASE_files.sh
 ./06_Mirror_Images_to_Private_Registry.sh
-./07_Configure_Secure_Private_Registry.sh
-./08_Install_Catalog_and_Operator.sh
+# ./07_Configure_Secure_Private_Registry.sh
+# ./08_Install_Catalog_and_Operator.sh
 # ./09_Configure_Allowed_Registries.sh
